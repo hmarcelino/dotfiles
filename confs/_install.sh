39048db41a81aa84c0d3303f68e03e2045cf2a8a
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2015-06-23 09:22:27 +0100

    Removed some directories creation.

--- confs/_install.sh
+++ confs/_install.sh
@@ -2,13 +2,6 @@
 
 source ../utils.sh
 
-if [[ -d ~/projects ]]; then
-    printInfo "Creating projects directory"
-    mkdir ~/projects
-    mkdir ~/projects/myprojects
-    mkdir ~/projects/company
-fi
-
 echo "======================================================="
 echo "Creating links "
 echo "======================================================="
